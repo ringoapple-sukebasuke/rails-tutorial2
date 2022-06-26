925c2a10e778a656449ce07fcddf92390caee3dc
Ubuntu <ubuntu@ip-172-31-80-214.ap-northeast-1.compute.internal>
2022-06-26 13:55:01 +0000

    Add hello

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "c325a6442cc8404292212db778ad7189.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 end
